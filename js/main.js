d64bc68c701c50327c6145e5627234679490e119
Sarah Jones <sarahjones1025@gmail.com>
2016-04-12 23:22:19 -0400

    Added interactive map

--- js/main.js
+++ js/main.js
@@ -24,3 +24,47 @@ $(document.body).on('click', function (e) {
 content.hide();
 
 $( ".trigger_2" ).trigger( "click" );
+
+// ^ How do I make it so that the screen always displays the active list item? Currently I have to scroll
+// to view the contents of each section.
+
+
+var mailer 		= document.getElementsByClassName( "mailer" );
+var openMailer 	= document.getElementById( "open_mailer" );
+var closeMailer = document.getElementById( "close_mailer" );
+
+var openUp = function openUp( e ){
+
+	mailer.className += ( "show" );
+	console.log( "working" );
+
+}
+
+var closeDown = function closeDown( e ){
+
+	mailer.classList.remove("show");
+	
+	e.preventDefault();
+}
+
+openMailer.addEventListener( "click", openUp );
+closeMailer.addEventListener( "click", closeDown );
+
+
+// ^ I am attempting to add the class "show" to this section. 
+//How do I set my checkbox to immediate mode so my click listener will work?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
